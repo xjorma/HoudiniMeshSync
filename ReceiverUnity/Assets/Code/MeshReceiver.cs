f2b2b87f8332b20f3158771e26b38806fbe1958c
David Gallardo <dgallardo@eidosmontreal.com>
2024-07-30 16:00:19 -0400

    Add vertex color support

--- ReceiverUnity/Assets/Code/MeshReceiver.cs
+++ ReceiverUnity/Assets/Code/MeshReceiver.cs
@@ -10,7 +10,7 @@ public class MeshReceiver : MonoBehaviour
     [SerializeField] private int port = 8666;
     private Thread receiverThread;
     private MeshFilter meshFilter;
-    private ConcurrentQueue<Tuple<int[],Vector3[], Vector3[]>> meshDataQueue = new ConcurrentQueue<Tuple<int [],Vector3[], Vector3[]>>();
+    private ConcurrentQueue<Tuple<int[],Vector3[], Vector3[], Color[]>> meshDataQueue = new ConcurrentQueue<Tuple<int [],Vector3[], Vector3[], Color[]>>();
 
     void Start()
     {
@@ -37,6 +37,23 @@ public class MeshReceiver : MonoBehaviour
         return result;
     }
 
+    Color[] ReadColorArray(byte[] buffer, ref int offset)
+    {
+        int size = BitConverter.ToInt32(buffer, offset) / 3;
+        offset += 4;
+        Color[] result = new Color[size];
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = new Color(
+                (float)buffer[offset] / 255.0f,
+                (float)buffer[offset + 1] / 255.0f,
+                (float)buffer[offset + 2] / 255.0f 
+            );
+            offset += 3;
+        }
+        return result;
+    }
+
     int[] ReadIntArray(byte[] buffer, ref int offset)
     {
         int size = BitConverter.ToInt32(buffer, offset);
@@ -59,6 +76,7 @@ public class MeshReceiver : MonoBehaviour
         {
             Vector3[] points = null;
             Vector3[] normals = null;
+            Color[] colors = null;
             int[] indices = null;
             using (TcpClient client = listener.AcceptTcpClient())
             {
@@ -98,9 +116,10 @@ public class MeshReceiver : MonoBehaviour
                     indices = ReadIntArray(dataBuffer, ref offset);
                     points = ReadVector3Array(dataBuffer, ref offset);
                     normals = ReadVector3Array(dataBuffer, ref offset);
+                    colors = ReadColorArray(dataBuffer, ref offset);
                 }
             }
-            meshDataQueue.Enqueue(Tuple.Create(indices,points, normals));
+            meshDataQueue.Enqueue(Tuple.Create(indices, points, normals, colors));
         }
     }
 
@@ -111,6 +130,7 @@ public class MeshReceiver : MonoBehaviour
             int[] indices = meshData.Item1;
             Vector3[] points = meshData.Item2;
             Vector3[] normals = meshData.Item3;
+            Color[] colors = meshData.Item4;
 
             Mesh mesh = new Mesh();
             if (points.Length > 65535)
@@ -120,6 +140,7 @@ public class MeshReceiver : MonoBehaviour
             mesh.vertices = points;
             mesh.normals = normals;
             mesh.triangles = indices;
+            mesh.colors = colors;
             mesh.RecalculateBounds();
             meshFilter.mesh = mesh;
         }
